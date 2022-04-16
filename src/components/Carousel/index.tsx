4fe14fb765b21614205ab6cf1b4efddc00bf2443
Shinjoku <deora.2@hotmail.com>
2022-04-15 21:01:06 -0300

    feat: add standard carousel basic layout

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,7 @@
+import { Card, Container } from "react-bootstrap";
 import Media from "../../types/Media/Media";
+import "./style";
+import { Standard } from "./style";
 
 interface CarouselProps {
   type: "standard" | "highlight";
@@ -6,8 +9,21 @@ interface CarouselProps {
   title?: string;
 }
 
-const Carousel = (props: CarouselProps) => (
-  <h3>{props.type === "standard" && props.title}</h3>
-);
+const Carousel = (props: CarouselProps) => {
+  const cards = props.items.map((i) => (
+    <Card>
+      <Card.Header>{i.title}</Card.Header>
+      <Card.Body>{i.description}</Card.Body>
+      <Card.Footer>{i.rating}</Card.Footer>
+    </Card>
+  ));
+
+  return (
+    <Standard>
+      <h3>{props.type === "standard" && props.title}</h3>
+      <Container>{cards}</Container>
+    </Standard>
+  );
+};
 
 export default Carousel;
---
+++ src/components/Carousel/style.tsx
@@ -0,0 +1,5 @@
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+
+export const Standard = styled(Container)``;
+export const Highlight = styled(Container)``;
